0c1fa398aaf5ff608593ba1773b8feef2481df9d
whityfenix <whityfenix@gmail.com>
2013-09-30 22:15:28 +0400

    Should be no sources here

--- doc/doxygen/make.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "/////////////////////////////////////////////////////////////////////"
-echo -e "Making unicomm documentation...\n"
-
-doxygen Doxyfile
-cp ../sources/template/css/navtree.css ../manuals/html/
-cp ../sources/template/images/body-bg.png ../manuals/html/
